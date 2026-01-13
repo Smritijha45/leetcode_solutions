d004d637c6fc7c1fbcc6b34ee374b076ce7e10aa
Smriti Jha <smritijha5487@gmail.com>
2026-01-13 22:49:27 +0530

    Create 238-ProductOfArrayExceptSelf

---
+++ 238-ProductOfArrayExceptSelf/238-ProductOfArrayExceptSelf.cpp
@@ -0,0 +1,24 @@
+// Last updated: 1/13/2026, 10:49:30 PM
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res(n, 1);
+
+        // Prefix product
+        int prefix = 1;
+        for (int i = 0; i < n; i++) {
+            res[i] = prefix;
+            prefix *= nums[i];
+        }
+
+        // Suffix product
+        int suffix = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] *= suffix;
+
+            suffix *= nums[i];
+        }
+        return res;
+    }
+};
