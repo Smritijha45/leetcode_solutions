6e7a3422678a9ba5afba0f1396a9e6498191eb53
Smriti Jha <smritijha5487@gmail.com>
2026-01-13 22:49:30 +0530

    Create 217-ContainsDuplicate

---
+++ 217-ContainsDuplicate/217-ContainsDuplicate.cpp
@@ -0,0 +1,17 @@
+// Last updated: 1/13/2026, 10:49:33 PM
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int, int> mpp;
+        for (int i = 0; i < n; i++) {
+            mpp[nums[i]]++;
+        }
+        for (auto it : mpp) {
+            if (it.second > 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
