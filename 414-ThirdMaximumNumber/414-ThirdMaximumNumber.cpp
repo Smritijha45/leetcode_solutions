98ccc0afc26f48d4f8e42317d5c9022ad82297d6
Smriti Jha <smritijha5487@gmail.com>
2026-01-13 22:49:20 +0530

    Create 414-ThirdMaximumNumber

---
+++ 414-ThirdMaximumNumber/414-ThirdMaximumNumber.cpp
@@ -0,0 +1,29 @@
+// Last updated: 1/13/2026, 10:49:23 PM
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        long v1= LONG_MIN;
+        long v2= LONG_MIN;
+        long v3= LONG_MIN;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==v1 || nums[i]==v2 || nums[i]==v3){
+                continue;
+            }
+            if(nums[i]>v1){
+                v3=v2;
+                v2=v1;
+                v1=nums[i];
+            }
+            else if(nums[i]>v2){
+                v3=v2;
+                v2=nums[i];
+            }
+            else if(nums[i]>v3){
+                v3=nums[i];
+            }
+        }
+        if(v3==LONG_MIN)return v1;
+        else return v3;
+        
+    }
+};
