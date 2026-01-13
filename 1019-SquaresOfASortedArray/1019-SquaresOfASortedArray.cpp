af4356f1332e2fbe355732e28d5a53e1b9905e83
Smriti Jha <smritijha5487@gmail.com>
2026-01-13 22:49:14 +0530

    Create 1019-SquaresOfASortedArray

---
+++ 1019-SquaresOfASortedArray/1019-SquaresOfASortedArray.cpp
@@ -0,0 +1,25 @@
+// Last updated: 1/13/2026, 10:49:17 PM
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n);
+        int left = 0, right = n - 1;
+        int pos = n - 1; 
+
+        while (left <= right) {
+            int leftSq = nums[left] * nums[left];
+            int rightSq = nums[right] * nums[right];
+
+            if (leftSq > rightSq) {
+                result[pos--] = leftSq;
+                left++;
+            } else {
+                result[pos--] = rightSq;
+                right--;
+            }
+        }
+
+        return result;
+    }
+};
