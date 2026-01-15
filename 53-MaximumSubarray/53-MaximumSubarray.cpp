8d06c47d3e842ad3542871037b365a3233c0aa50
Smriti Jha <smritijha5487@gmail.com>
2026-01-15 11:45:56 +0530

    File updated at 1/15/2026, 11:46:00 AM

--- 53-MaximumSubarray/53-MaximumSubarray.cpp
+++ 53-MaximumSubarray/53-MaximumSubarray.cpp
@@ -1,14 +1,14 @@
-// Last updated: 1/13/2026, 10:49:48 PM
-class Solution {
-public:
-    int maxSubArray(vector<int>& nums) {
-        int n = nums.size();
-        int currentSum = nums[0];
-        int maxSum = nums[0];
-        for (int i = 1; i < n; i++) {
-            currentSum = max(nums[i], currentSum + nums[i]);
-            maxSum = max(maxSum, currentSum);
-        }
-        return maxSum;
-    }
-};
+// Last updated: 1/15/2026, 11:46:00 AM
+1class Solution {
+2public:
+3    int maxSubArray(vector<int>& nums) {
+4        int n = nums.size();
+5        int currentSum = nums[0];
+6        int maxSum = nums[0];
+7        for (int i = 1; i < n; i++) {
+8            currentSum = max(nums[i], currentSum + nums[i]);
+9            maxSum = max(maxSum, currentSum);
+10        }
+11        return maxSum;
+12    }
+13};
